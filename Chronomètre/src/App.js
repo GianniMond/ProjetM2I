de06a042093b10ca9e4dd5c1b0f2be5932299c21
Gianni Mondello <gianni.mondello@hotmail.fr>
2023-03-16 11:43:19 +0100

    ajout du projet chronometre

---
+++ Chronomètre/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Minuteur from './views/Minuteur.jsx';
+
+function App() {
+  return (
+    <div className="App">
+        <Minuteur/>
+      </div>
+  );
+}
+
+export default App;
